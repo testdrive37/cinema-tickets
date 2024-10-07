46e5d0286c4e33171c25dbda33709b9a1b8b8f39
testdrive37 <testdrive2374@gmail.com>
2024-10-07 16:02:50 +0100

    Revert "init commit"
    This reverts commit 48e8aee341313c5108f7d357ad446c9848936626.

--- cinema-tickets-java/src/main/java/uk/gov/dwp/uc/pairtest/exception/InvalidPurchaseException.java
+++ cinema-tickets-java/src/main/java/uk/gov/dwp/uc/pairtest/exception/InvalidPurchaseException.java
@@ -2,20 +2,4 @@ package uk.gov.dwp.uc.pairtest.exception;
 
 public class InvalidPurchaseException extends RuntimeException {
 
-    public InvalidPurchaseException() {
-        super("Invalid ticket purchase request.");
-    }
-
-    public InvalidPurchaseException(String message) {
-        super(message);
-    }
-
-    public InvalidPurchaseException(String message, Throwable cause) {
-        super(message, cause);
-    }
-
-    public InvalidPurchaseException(Throwable cause) {
-        super(cause);
-    }
 }
-
